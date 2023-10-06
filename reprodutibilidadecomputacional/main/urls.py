02521ea9e07585ab598722841a41b5498e6469e0
JNovoletti <joao.novoletti@gmail.com>
2023-10-06 12:15:18 -0300

    updating branch

--- reprodutibilidadecomputacional/main/urls.py
+++ reprodutibilidadecomputacional/main/urls.py
@@ -1,5 +1,6 @@
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url, include
 
 from . import views
 
@@ -9,9 +10,11 @@ urlpatterns = [
     path('instafeed/', views.instafeed, name='instafeed'),
     path('quemsomos/', views.quemsomos, name='quemsomos'),
     path('contato/', views.contact, name='contato'),
-    path('ebook/', views.ebook, name='ebook'),
+    #path('ebook/', views.ebook, name='ebook'),
+    url(r'^docs/', include('django_mkdocs.urls', namespace='documentation')),
     path('apoio/', views.apoio, name='apoio'),
     path('participe/', views.participe, name='participe'),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- reprodutibilidadecomputacional/reprodutibilidadecomputacional/urls.py
+++ reprodutibilidadecomputacional/reprodutibilidadecomputacional/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("", include('main.urls')),
     path("admin/", admin.site.urls),
-    path('tinymce/', include('tinymce.urls'))
+    path('tinymce/', include('tinymce.urls'))    
 ]
